7ebe24963fcf7d3f69766e5721a8357f645780cf
Will Murray <will.murray@gmail.com>
2013-09-22 02:37:30 -0400

    JavaScript Functionality
    Added onclick listeners and  classes to HTML doc, added for loop to JS
    doc. Functionality of show/hide HTML elements is now live.

--- application.js
+++ application.js
@@ -1,8 +1,12 @@
 function navigate(id) {
-  var section = document.getElementById(id);
-  if(section.style.display == "none") {
-    section.style.display="block";
-  } else {
-    section.style.display="none";
+  var section = document.getElementsByClassName("section");
+  for (var i = 0; i < section.length; i++) {
+    section[i].style.display = 'none';
   }
-}
+  var page = document.getElementById(id);
+  if(page.style.display == "none") {
+    page.style.display="block";
+  } else {
+    page.style.display="none";
+  }
+};
